cf6318c2616fffa4da90e840e67dfb210afadf87
Cazuist <sovrus@mail.ru>
2020-07-24 13:03:52 +0300

    Edit async mode of class

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -2,12 +2,15 @@ import json from './parser';
 import read from './reader';
 
 export default class GameSavingLoader {
-  static load() {
-    return (async () => {
+  // eslint-disable-next-line consistent-return
+  static async load() {
+    try {
       const data = await read();
       const value = await json(data);
 
       return value;
-    })();
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
